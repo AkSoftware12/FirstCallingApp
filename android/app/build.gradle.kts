fab866176efc75058597269f73674d55793cdfe4
Ravikant saini <146403818+AkSoftware12@users.noreply.github.com>
2025-11-28 17:37:31 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,26 +1,24 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
-import java.util.Properties
-        import java.io.FileInputStream
 
-val keystorePropertiesFile = rootProject.file("key.properties")
+import java.util.Properties
+import java.io.FileInputStream
+
+// Load keystore
 val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("android/app/key.properties")
 
 if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
-
-
 android {
     namespace = "com.firstcallingapp.firstcallingapp"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
-
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -32,34 +30,29 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.firstcallingapp.firstcallingapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+            storeFile = file(keystoreProperties["storeFile"]!!.toString())
+            storePassword = keystoreProperties["storePassword"]!!.toString()
+            keyAlias = keystoreProperties["keyAlias"]!!.toString()
+            keyPassword = keystoreProperties["keyPassword"]!!.toString()
         }
     }
 
     buildTypes {
         getByName("release") {
+            isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("release")
         }
     }
-
 }
-
 
 flutter {
     source = "../.."
